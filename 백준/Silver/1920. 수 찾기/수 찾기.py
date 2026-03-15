14d8b52da6b834129be3ba4b4efaac0f64d6b0ce
Hahm Seonyul <mara71@office.skhu.ac.kr>
2026-03-15 20:07:01 +0900

    [Silver IV] Title: 수 찾기, Time: 160 ms, Memory: 52272 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,9 +1,9 @@
-N = int(input())
+N =int(input())
 A = set(map(int,input().split()))
 M = int(input())
 B = list(map(int,input().split()))
 for i in range(M):
     if B[i] in A:
-         print(1)
+        print(1)
     else:
-         print(0)
+        print(0)
